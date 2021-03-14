15b51f236dfaef360c616f6f732e5a8a982dc0d3
Ubuntu <ubuntu@ip-172-31-23-152.ec2.internal>
2021-03-14 19:02:16 +0000

    Set up Terraformfile

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+# You may configure the profile name and Region here
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "ToDoApp" {
+  ami           = "ami-04f3901ef34fb506c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TodoAppInstance"
+  }
+}
