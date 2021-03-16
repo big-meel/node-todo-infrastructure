2e57f7c4644220fa54ec085a688d6c68059e1d08
big-meel <big.meel@zoho.com>
2021-03-16 07:27:44 -0500

    Add security group to tf file

--- main.tf
+++ main.tf
@@ -8,6 +8,8 @@ terraform {
 }
 
 # You may configure the profile name and Region here
+# If aws cli is set up then you may add the profile name
+# otherwise privide access credentials in place of 'profile'
 provider "aws" {
   profile = "default"
   region  = "us-east-2"
@@ -16,10 +18,29 @@ provider "aws" {
 resource "aws_instance" "ToDoApp" {
   ami           = "ami-04f3901ef34fb506c"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "aws_security_group.node-sg.id" ]
 
   tags = {
     Name = "TodoAppInstance"
   }
+}
+
+resource "aws_security_group" "node-sg" {
+  name = "jameel-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
 
